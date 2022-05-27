9ab465bd5508a2b0b162435e31bef75e7431fbab
Nikita Boev <nekitcudder@yandex.ru>
2022-05-27 20:49:58 +0300

    feat: создать работу с popup открыть/закрыть внести данные

---
+++ scripts/index.js
@@ -0,0 +1,28 @@
+let popup = document.querySelector('.popup');
+let buttonOpen = document.querySelector('.profile__button-edit');
+let buttonSave = document.querySelector('.popup__button-save');
+let formPopup = document.querySelector('.popup__form');
+let nameInput = document.querySelector('.popup__name');
+let captionInput = document.querySelector('.popup__caption');
+let profileName = document.querySelector('.profile__name');
+let profileCaption = document.querySelector('.profile__caption');
+let buttonClose = document.querySelector('.popup__button-close')
+
+function popupOpen() {
+  popup.classList.add('popup_opened');
+}
+buttonOpen.addEventListener('click', popupOpen);
+
+function popupClose() {
+  popup.classList.remove('popup_opened');
+}
+buttonClose.addEventListener('click', popupClose);
+
+function formSubmit(evt) {
+  evt.preventDefault();
+  profileName.textContent = nameInput.value;
+  profileCaption.textContent = captionInput.value;
+}
+formPopup.addEventListener('submit', formSubmit);
+buttonSave.addEventListener('click', formSubmit);
+buttonSave.addEventListener('click', popupClose);
