907dc08a2a2d1c4953f1c7d16e75bfa9d78d9d28
Nikita Boev <nekitcudder@yandex.ru>
2022-05-30 14:53:46 +0300

    fix: удаление лишних строк в js

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,8 @@
 let popup = document.querySelector('.popup');
 let buttonOpen = document.querySelector('.profile__button-edit');
-let buttonSave = document.querySelector('.popup__button-save');
 let formPopup = document.querySelector('.popup__form');
-let nameInput = document.querySelector('.popup__name');
-let captionInput = document.querySelector('.popup__caption');
+let nameInput = document.querySelector('.popup__input_text_name');
+let captionInput = document.querySelector('.popup__input_text_caption');
 let profileName = document.querySelector('.profile__name');
 let profileCaption = document.querySelector('.profile__caption');
 let buttonClose = document.querySelector('.popup__button-close');
@@ -13,19 +12,18 @@ function popupOpen() {
   nameInput.value = profileName.textContent;
   captionInput.value = profileCaption.textContent;
 }
-buttonOpen.addEventListener('click', popupOpen);
 
 function popupClose() {
   popup.classList.remove('popup_opened');
 }
-buttonClose.addEventListener('click', popupClose);
 
 function formSubmit(evt) {
   evt.preventDefault();
   profileName.textContent = nameInput.value;
   profileCaption.textContent = captionInput.value;
+  popupClose();
 }
 
+buttonOpen.addEventListener('click', popupOpen);
+buttonClose.addEventListener('click', popupClose);
 formPopup.addEventListener('submit', formSubmit);
-buttonSave.addEventListener('click', formSubmit);
-buttonSave.addEventListener('click', popupClose);
