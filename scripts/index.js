194eab743f1fe8de4b9be899274514fa6e62c496
Nikita Boev <nekitcudder@yandex.ru>
2022-06-10 20:35:03 +0300

    feat: создать popup добавления новой карточки

--- scripts/index.js
+++ scripts/index.js
@@ -1,29 +1,81 @@
-let popup = document.querySelector('.popup');
-let buttonOpen = document.querySelector('.profile__button-edit');
-let formPopup = document.querySelector('.popup__form');
-let nameInput = document.querySelector('.popup__input_text_name');
-let captionInput = document.querySelector('.popup__input_text_caption');
-let profileName = document.querySelector('.profile__name');
-let profileCaption = document.querySelector('.profile__caption');
-let buttonClose = document.querySelector('.popup__button-close');
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
 
-function popupOpen() {
-  popup.classList.add('popup_opened');
-  nameInput.value = profileName.textContent;
-  captionInput.value = profileCaption.textContent;
+const popup = document.querySelector('.popup');
+const popupEP = document.querySelector('.popup_ep');
+const popupAC = document.querySelector('.popup_ac');
+const buttonEditProfile = document.querySelector('.profile__button-edit');
+const buttonAddCard = document.querySelector('.profile__button-add');
+const formPopupProfile = document.querySelector('.popup__form');
+const nameInput = document.querySelector('.popup__input_text_name');
+const captionInput = document.querySelector('.popup__input_text_caption');
+const profileName = document.querySelector('.profile__name');
+const profileCaption = document.querySelector('.profile__caption');
+const buttonCloseProfile = popupEP.querySelector('.popup__button-close');
+const buttonCloseCard = popupAC.querySelector('.popup__button-close');
+
+
+//функция открытия попапа
+const popupOpen = (open) => {
+  open.classList.add('popup_opened');
 }
-
-function popupClose() {
-  popup.classList.remove('popup_opened');
+//функция закрытия попапа
+const popupClose = (close) => {
+  close.classList.remove('popup_opened');
 }
-
-function formSubmit(evt) {
+//функция изменения данных профиля
+const formSubmit = (evt) => {
   evt.preventDefault();
   profileName.textContent = nameInput.value;
   profileCaption.textContent = captionInput.value;
-  popupClose();
+  popupClose(popupEP);
 }
 
-buttonOpen.addEventListener('click', popupOpen);
-buttonClose.addEventListener('click', popupClose);
-formPopup.addEventListener('submit', formSubmit);
+
+
+
+
+//обработчик событий открытия попапов
+buttonEditProfile.addEventListener('click', () => {
+  popupOpen(popupEP);
+  nameInput.value = profileName.textContent;
+  captionInput.value = profileCaption.textContent;
+});
+buttonAddCard.addEventListener('click', () => {
+  popupOpen(popupAC);
+});
+
+//обработчик событий закрытия попапов
+buttonCloseProfile.addEventListener('click', () => {
+  popupClose(popupEP);
+});
+buttonCloseCard.addEventListener('click', () => {
+  popupClose(popupAC);
+});
+
+//обработчик событий сохранения данных
+formPopupProfile.addEventListener('submit', formSubmit);
